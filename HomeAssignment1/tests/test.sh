1bbef8da96d6678aa69ba73d676d719bfe7a2ce5
ArtemChurilov <st130184@student.spbu.ru>
2025-05-03 20:44:56 +0300

    add first python ci test to encoder

---
+++ HomeAssignment1/tests/test.sh
@@ -0,0 +1,14 @@
+set -e
+
+dd if=/dev/urandom of=test.bin bs=1024 count=10 status=none
+
+python3 -c "import base64; print(base64.a85encode(open('test.bin','rb').read()).decode())" > py.a85
+
+./ascii85 test.bin > our.a85
+
+if cmp -b py_stripped.a85 our_stripped.a85; then
+    echo "Test completed right."
+else
+    echo "Test failed"
+    exit 1
+fi
